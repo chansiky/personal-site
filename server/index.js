9f7d371b807421e42f9aded1b83a5c34c666390a
chansiky <chansiky@gmail.com>
2018-09-18 12:00:43 -0500

    port change

--- server/index.js
+++ server/index.js
@@ -28,6 +28,9 @@ if (process.env.NODE_ENV === 'test') {
  */
 if (process.env.NODE_ENV !== 'production') require('../secrets')
 
+
+//.listen(process.env.PORT || 5000)
+
 // passport registration
 passport.serializeUser((user, done) => done(null, user.id))
 
@@ -96,7 +99,7 @@ const createApp = () => {
 
 const startListening = () => {
   // start listening (and create a 'server' object representing our server)
-  const server = app.listen(PORT, () =>
+  const server = app.listen(process.env.PORT || 8080, () =>
     console.log(`Mixing it up on port ${PORT}`)
   )
 
